4c6d9e5ffc582028457c0292d10cf4b4d71727d9
ioanabirsan <ioana.c.birsan@gmail.com>
2018-04-15 09:48:24 +0300

    Add operation documentation in Swagger

--- src/main/java/ro/infoiasi/ip/easylearn/submission/controller/SubmissionController.java
+++ src/main/java/ro/infoiasi/ip/easylearn/submission/controller/SubmissionController.java
@@ -1,5 +1,7 @@
 package ro.infoiasi.ip.easylearn.submission.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import ro.infoiasi.ip.easylearn.submission.model.SubmissionResponse;
 import ro.infoiasi.ip.easylearn.submission.service.SubmissionService;
@@ -11,6 +13,7 @@ import java.util.List;
 
 
 @RestController
+@Api(value = "submissions", description = "Operations pertaining to the manipulations of submissions")
 public class SubmissionController {
     private SubmissionService submissionService;
 
@@ -20,18 +23,21 @@ public class SubmissionController {
 
     @RequestMapping(path = "/submissions", method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation(value = "View the information about the submissions entered in the system")
     public List<Submission> submissions() {
         return submissionService.findAll();
     }
 
     @RequestMapping(path = "/submissions/{id}", method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation(value = "View the information about a particular submission")
     public Submission submissions(@PathVariable Long id) {
         return submissionService.findById(id);
     }
 
     @RequestMapping(path = "/submit", method = RequestMethod.POST)
     @ResponseBody
+    @ApiOperation(value = "Send a submission for processing")
     public SubmissionResponse submit(@RequestBody Submission submission) {
         return submissionService.submit(submission);
     }
