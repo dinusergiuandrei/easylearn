0733f89c340a38a71d455c10b29bb16d32505661
Dinu Sergiu-Andrei <dinusergiuandrei@yahoo.com>
2018-04-18 13:00:57 +0300

    Added output

--- src/main/java/ro/infoiasi/ip/easylearn/EasyLearnApplication.java
+++ src/main/java/ro/infoiasi/ip/easylearn/EasyLearnApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EasyLearnApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EasyLearnApplication.class, args);
 	}
