2343146cf95c99ea253f157151ff8884adfbc417
Oana <neaguoanaandreea@gmail.com>
2018-05-22 00:06:39 +0300

    [Tests] SubmissionTest

--- src/test/java/ro/infoiasi/ip/easylearn/management/model/SubmissionTestTest.java
+++ src/test/java/ro/infoiasi/ip/easylearn/management/model/SubmissionTestTest.java
@@ -1,20 +1,33 @@
 package ro.infoiasi.ip.easylearn.management.model;
 
 import org.junit.Test;
+import ro.infoiasi.ip.easylearn.submission.model.Submission;
+
+import java.lang.reflect.Field;
 
 import static org.junit.Assert.*;
 
 public class SubmissionTestTest {
 
     @Test
-    public void setSubmissionID() {
+    public void testSetter_setSubmissionID() throws NoSuchFieldException, IllegalAccessException {
+        final SubmissionTest testSubmissionTest = new SubmissionTest();
+        testSubmissionTest.setSubmissionID(123056);
+
+        final Field field = testSubmissionTest.getClass().getDeclaredField("submissionID");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(testSubmissionTest), (long)123056);
     }
 
     @Test
-    public void setTestID() {
+    public void testSetter_setTestID() throws NoSuchFieldException, IllegalAccessException {
+        final SubmissionTest testSubmissionTest = new SubmissionTest();
+        testSubmissionTest.setTestID(5642);
+
+        final Field field = testSubmissionTest.getClass().getDeclaredField("testID");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(testSubmissionTest), (long)5642);
     }
 
-    @Test
-    public void setPassed() {
-    }
+
 }
