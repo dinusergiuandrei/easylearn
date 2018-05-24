95d4d7a72026e6edcf42bc12a4ebaac419cc359c
Calin Zapan <calinzapan@gmail.com>
2018-05-24 19:14:07 +0300

    Start of Integration
    Auth Service
    Notification Service

---
+++ src/main/resources/static/src/app/services/notification.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class NotificationService {
+  notification = null;
+  constructor() {}
+
+  push(notification: any) {
+    notification.interval = setTimeout(() => {
+      this.remove();
+    }, 5000);
+    this.notification = notification;
+  }
+
+  remove() {
+    if (this.notification) {
+      clearInterval(this.notification.interval);
+      this.notification = false;
+    }
+  }
+}
