741e4bcbfb21c6d7c34083fbdc933ca0c2bfa25a
Robert Maftei <mafrobert97@gmail.com>
2018-05-28 00:02:24 +0300

    Categories Refactored

--- src/main/resources/static/src/app/components/public/categories/categories.component.ts
+++ src/main/resources/static/src/app/components/public/categories/categories.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: { name: string; path: string; route: string; }[];
 
-  ngOnInit() {
+  constructor() {
+    this.categories = [
+      {
+        name: 'Algorithms',
+        path: '../../../../assets/images/categories/algorithms.png',
+        route: '/problems-category/algorithms'
+      },
+      {
+        name: 'Data Structures',
+        path: '../../../../assets/images/categories/data-structures.png',
+        route: '/problems-category/data_structures'
+      },
+      {
+        name: 'Mathematics',
+        path: '../../../../assets/images/categories/mathematics.png',
+        route: '/problems-category/mathematics'
+      }
+    ];
   }
+
+  ngOnInit() { }
 
 }
