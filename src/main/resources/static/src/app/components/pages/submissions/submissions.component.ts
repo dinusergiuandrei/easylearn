e726135f1d097c7fd06b38c20c2202c21cfe6eaa
Calin Zapan <calinzapan@gmail.com>
2018-05-27 20:00:34 +0300

    Submissions Component
    Pentru Florin
    Lenesule, creaza-ti singer componentele si nu ma ma mai bate la cap

---
+++ src/main/resources/static/src/app/components/pages/submissions/submissions.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-submissions',
+    styleUrls: ['./submissions.component.scss'],
+    templateUrl: './submissions.component.html',
+})
+export class SubmissionsComponent{
+
+    constructor() { }
+  
+    ngOnInit() {
+    }
+  
+  }
