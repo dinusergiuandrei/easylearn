48e57e42df30130cb348f7a2245b6202525f6d02
BogdanConstantin <constantin.bogdan.mirza@gmail.com>
2018-05-27 14:00:21 +0300

    user validator

--- src/main/java/ro/infoiasi/ip/easylearn/user/validation/UserValidator.java
+++ src/main/java/ro/infoiasi/ip/easylearn/user/validation/UserValidator.java
@@ -5,32 +5,40 @@ import ro.infoiasi.ip.easylearn.user.model.User;
 
 @Component
 public class UserValidator {
-    public UserValidator(){};
-    public boolean validateUserUpdate(User user){
-        String pattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
-        if (user.getName().length() < 3 || user.getName() == null)
-            return false;
-        if (user.getFirstName().length() < 3 || user.getFirstName() == null)
-            return false;
-        if ((!user.getEmail().matches(pattern)) || user.getEmail() == null)
-            return false;
-        return true;
-    };
+    public UserValidator() {
+    }
 
-    public boolean validateUserRegister(User user){
+    ;
+
+    public boolean validateUserUpdate(User user) {
         String pattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
-        if (user.getName().length() < 3 || user.getName() == null)
+        if (user.getName() == null || user.getName().length() < 3)
             return false;
-        if (user.getFirstName().length() < 3 || user.getFirstName() == null)
+        if (user.getFirstName() == null || user.getFirstName().length() < 3)
             return false;
-        if ((!user.getEmail().matches(pattern)) || user.getEmail() == null)
-            return false;
-        if (user.getPassword().length() < 4 || user.getPassword() == null)
-            return false;
-        if (user.getSecretAnswer().length() < 4 || user.getSecretAnswer() == null)
-            return false;
-        if (user.getSecretQuestion().length() < 4 || user.getSecretQuestion() == null)
+        if (user.getEmail() == null || (!user.getEmail().matches(pattern)))
             return false;
         return true;
-    };
+    }
+
+    ;
+
+    public boolean validateUserRegister(User user) {
+        String pattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+        if (user.getName() == null || user.getName().length() < 3)
+            return false;
+        if (user.getFirstName() == null || user.getFirstName().length() < 3)
+            return false;
+        if (user.getEmail() == null || (!user.getEmail().matches(pattern)))
+            return false;
+        if (user.getPassword() == null || user.getPassword().length() < 4)
+            return false;
+        if (user.getSecretAnswer() == null || user.getSecretAnswer().length() < 4)
+            return false;
+        if (user.getSecretQuestion() == null || user.getSecretQuestion().length() < 4)
+            return false;
+        return true;
+    }
+
+    ;
 }
