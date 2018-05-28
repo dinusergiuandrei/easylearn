ffccd76c5a34c5f53697590b193eed7baf25c734
Gabriel Ceicoschi <ceicoschi.gabriel@gmail.com>
2018-05-29 00:19:19 +0300

    submission table

--- src/main/resources/static/src/app/components/pages/submissions/submissions.component.ts
+++ src/main/resources/static/src/app/components/pages/submissions/submissions.component.ts
@@ -1,31 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { SubmitSolutionService, ProblemService } from '../../../services';
-import { SubmissionRequestModel, ProblemModel, SourceModel, SubmissionModel } from '../../../shared';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SubmitSolutionService} from '../../../services';
+import {MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
+import {ProblemModel} from '../../../shared';
+import {UserData} from '../top/top.component';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
-    selector: 'app-submissions',
-    styleUrls: ['./submissions.component.scss'],
-    templateUrl: './submissions.component.html',
+  selector: 'app-submissions',
+  styleUrls: ['./submissions.component.scss'],
+  templateUrl: './submissions.component.html',
 })
 export class SubmissionsComponent implements OnInit {
+  displayedColumns = ['id', 'problem', 'user', 'date', 'language', 'state', 'link'];
+  dataSource: MatTableDataSource<ProblemModel>;
 
-    constructor(private route: ActivatedRoute, private router: Router, private submissionService: SubmitSolutionService) { }
+  constructor(private route: ActivatedRoute, private router: Router, private submissionService: SubmitSolutionService) {
+  }
 
-    public problemId;
-    public submissions; // = new Array<SubmissionModel>();
+  public problemId;
+  public submissions;
+  public sub;
 
-    ngOnInit() {
-        this.route.queryParams.subscribe(params => { this.problemId = params['problemId']; });
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.problemId = params['problemId'];
+    });
+    this.submissionService.getSubmissionsByProblem(this.problemId).subscribe((result: ProblemModel[]) => {
+        this.submissions = result;
+        this.dataSource = new MatTableDataSource(result);
+        console.log(this.dataSource);
+      },
+      err => {
+        this.router.navigate(['/403']);
+      });
+  }
+}
 
-        this.submissionService.getSubmissionsByProblem(this.problemId).subscribe((result: ProblemModel) => {
-            this.submissions = result;
-            console.log(this.submissions);
-        },
-            err => {
-                this.router.navigate(['/403']);
-            });
-    }
-
+export interface Submissions {
+  id: number;
+  problem: string;
+  user: string;
+  language: string;
+  date: string;
+  state: string;
+  link: string;
 }
