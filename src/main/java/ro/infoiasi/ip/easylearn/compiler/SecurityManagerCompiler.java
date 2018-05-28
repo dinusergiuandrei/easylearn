85e030dca48747bab80ade8d2d2bfde2015174c1
Dinu Sergiu-Andrei <dinusergiuandrei@yahoo.com>
2018-05-28 15:29:57 +0300

    Improved output collecting method.

--- src/main/java/ro/infoiasi/ip/easylearn/compiler/SecurityManagerCompiler.java
+++ src/main/java/ro/infoiasi/ip/easylearn/compiler/SecurityManagerCompiler.java
@@ -123,8 +123,8 @@ public class SecurityManagerCompiler extends Compiler {
         }
 
         //return collectOutput(outputDirectoryPath);
-        //return Output.getOutputFromMessage(defaultCompilerOutput.getOutput());
-        return defaultCompilerOutput;
+        return Output.getOutputFromMessage(defaultCompilerOutput.getOutput());
+        //return defaultCompilerOutput;
     }
 
     private Output collectOutput(String outputDirectoryPath) throws IOException {
